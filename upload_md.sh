6f23dd9558233c2b59882a23e9df2139b99532a0
ganeshtaru <ganeshtaru91@gmail.com>
2025-04-07 13:00:26 +0530

    updated prop

--- upload_md.sh
+++ upload_md.sh
@@ -9,18 +9,18 @@ fi
 read -p "Enter IAM username: " iam_user
 
 # This username is hardcoded in sql scripts
-DB_PWD=$(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
-DB_HOST='mdg-db.cvi0iu2wkmhx.ap-south-1.rds.amazonaws.com'
+DB_PWD='dupp8MzNvD'
+DB_HOST='postgres.mdg-pilot.mosip.net'
 DB_PORT=5432
 XLS=mosip_master/xlsx
 
 while true; do
     read -p "WARNING: All existing masterdata will be erased. Are you sure?(Y/n) " yn
-    if [ $yn == "Y" ]
+    if [ $yn = "Y" ]
       then
         echo Uploading ..
         cd lib
-        python upload_masterdata.py $DB_HOST $DB_PWD $iam_user ../$XLS
+        python3 upload_masterdata.py $DB_HOST $DB_PWD $iam_user ../$XLS
         break
       else
         break
